a614ec0f0b0ae4fe7f442e51d78ec6a9b1cc14fb
Konata9 <maochichen@sunmi.com>
2019-06-05 17:54:11 +0800

    增加umd

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,43 @@
-const path = require('path')
+const path = require("path");
 
 const entryFile = {
-  prod: './index.js',
-  test: './test/storage.test.js'
-}
+  prod: "./index.js",
+  test: "./test/storage.test.js"
+};
 const outPutPath = {
-  prod: { path: path.resolve(__dirname, './dist'), filename: 'index.js' },
-  test: { path: path.resolve(__dirname, './test'), filename: 'storage.test.bundle.js' }
-}
+  prod: {path: path.resolve(__dirname, "./dist"), filename: "index.js"},
+  test: {
+    path: path.resolve(__dirname, "./test"),
+    filename: "storage.test.bundle.js"
+  }
+};
 
 module.exports = (env) => {
-  const currentEnv = env.NODE_ENV
-  console.log(`Now in ENV: ${currentEnv}.`)
+  const currentEnv = env.NODE_ENV;
+  console.log(`Now in ENV: ${currentEnv}.`);
 
   return {
-    mode: 'development',
+    mode: "development",
     entry: entryFile[currentEnv],
     output: {
       path: outPutPath[currentEnv].path,
-      filename: outPutPath[currentEnv].filename
+      filename: outPutPath[currentEnv].filename,
+      libraryTarget: "umd"
     },
-    devtool: 'source-map',
+    devtool: "source-map",
     module: {
       rules: [
         {
           test: /\.js$/,
-          include: [path.resolve(__dirname, './src')],
-          exclude: [path.resolve(__dirname, './node_modules')],
+          include: [path.resolve(__dirname, "./src")],
+          exclude: [path.resolve(__dirname, "./node_modules")],
           use: [
             {
-              loader: 'babel-loader'
+              loader: "babel-loader"
             }
           ]
         }
       ]
     }
-  }
-}
+  };
+};
